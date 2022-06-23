e0a1cd948ec16e8b2d3fc0b10eb47ee71a691132
Ethan <ethanbaird94@gmail.com>
2022-06-23 15:58:18 +0100

    refactored get_completed_tasks and get_uncomplleted_tasks

--- start_code/modules/task_list.py
+++ start_code/modules/task_list.py
@@ -8,21 +8,44 @@ tasks = [
 
 # Functions to complete:
 
+
+def get_tasks_by_status(list, status):
+    if status == "completed":
+        status = True
+    elif status == "uncompleted":
+        status = False
+    tasks_by_status = []
+    for task in list:
+        if task["completed"] == status:
+            tasks_by_status.append(task["description"])
+        else:
+            pass
+    return tasks_by_status
+
 ## Get a list of uncompleted tasks
 def get_uncompleted_tasks(list):
-    uncompleted_tasks = []
-    for task in list:
-        if task["completed"] == False:
-            uncompleted_tasks.append(task["description"])
+    # uncompleted_tasks = []
+    # for task in list:
+    #     if task["completed"] == False:
+    #         uncompleted_tasks.append(task["description"])
+    # return uncompleted_tasks
+    uncompleted_tasks = get_tasks_by_status(list, "uncompleted")
     return uncompleted_tasks
+    
+
+print(get_uncompleted_tasks(tasks))
 
 ## Get a list of completed tasks
 def get_completed_tasks(list):
-    completed_tasks = []
-    for task in list:
-        if task["completed"] == True:
-            completed_tasks.append(task["description"])
+    # completed_tasks = []
+    # for task in list:
+    #     if task["completed"] == True:
+    #         completed_tasks.append(task["description"])
+    # return completed_tasks
+    completed_tasks = get_tasks_by_status(list, "completed")
     return completed_tasks
+
+print(get_completed_tasks(tasks))
 
 ## Get tasks where time_taken is at least a given time
 def get_tasks_taking_at_least(list, time):
@@ -45,21 +68,21 @@ def get_task_with_description(list, description):
 # Extention (Function): 
 
 ## Get a list of tasks by status
-def get_tasks_by_status(list, status):
-    if status == "completed":
-        status = True
-    elif status == "uncompleted":
-        status = False
-    tasks_by_status = []
-    for task in list:
-        if task["completed"] == status:
-            tasks_by_status.append(task["description"])
-        else:
-            pass
-    return tasks_by_status
+# def get_tasks_by_status(list, status):
+#     if status == "completed":
+#         status = True
+#     elif status == "uncompleted":
+#         status = False
+#     tasks_by_status = []
+#     for task in list:
+#         if task["completed"] == status:
+#             tasks_by_status.append(task["description"])
+#         else:
+#             pass
+#     return tasks_by_status
 
-print(get_tasks_by_status(tasks, "completed"))
-print(get_tasks_by_status(tasks, "uncompleted"))
+# print(get_tasks_by_status(tasks, "completed"))
+# print(get_tasks_by_status(tasks, "uncompleted"))
 
 def mark_task_complete(task):
     task["completed"] = True
